6775890c9965568e991fbabb5994964afad934e8
varun-kajarekar <91301190+varun-kajarekar@users.noreply.github.com>
2021-12-30 22:17:15 +0530

    2

---
+++ Code Drive Division 3 (Rated)/Favourite String of Chef.java
@@ -0,0 +1,96 @@
+import java.util.*;
+import java.lang.*;
+import java.math.BigInteger;
+import java.time.Period;
+import java.io.*;
+
+class Codechef
+{   
+    static FastReader vk = new FastReader();
+
+    
+    public static void solve(int t){
+        while(t-->0){
+            int N = vk.nextInt();
+            String str = vk.nextLine();
+            int a =str.indexOf("code");
+            int b = str.indexOf("chef");
+            if(a<b){
+                System.out.println("AC");
+            }
+            else{
+                System.out.println("WA");
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		
+        int t =vk.nextInt();
+        solve(t);
+        // // System.out.println();
+        
+        
+    }
+    
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
